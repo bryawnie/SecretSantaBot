a3c579e2b55134a6ea2d7d5df6e5c9c336551cd9
Bryan Ortiz <bryan.ortiz@ug.uchile.cl>
2022-12-01 15:08:32 -0300

    Fix handling errors.

--- runner.py
+++ runner.py
@@ -11,17 +11,23 @@ TOKEN = "YOUR_TOKEN"
 updater = Updater(TOKEN, use_context=True)
 DATABASE = "db.sqlite"
 
+help_text = (
+    "- Utiliza /cambiarJuego <id_juego> para cambiar de juego\n"+
+    "- Utiliza /meGustaria <regalo> para agregar un tipo de regalo que te gustaría recibir\n"+
+    "- Utiliza /noMeGustaria <no_regalo> para agregar un tipo de regalo que no te gustaría recibir"
+)
+
 def start(update: Update, context: CallbackContext):
     con = connect(DATABASE)
     cur = con.cursor()
 
     user = update.message.from_user
-    if cur.execute(f"SELECT * FROM friends WHERE id={user.id}").fetchone() is None:
+    if cur.execute(f"SELECT * FROM friends WHERE user_id={user.id}").fetchone() is None:
         cur.execute(f"INSERT INTO friends VALUES({user.id}, '{user.first_name} {user.last_name}', 0, '', '')")
         con.commit()
-        update.message.reply_text(f"Hola {user.first_name}!, Juguemos al amigo secreto :)")
+        update.message.reply_text(f"Hola {user.first_name}!, Juguemos al amigo secreto :)\n\n" + help_text)
     else:
-        update.message.reply_text(f"Hola {user.first_name}! te encuentras registradx para jugar :D")
+        update.message.reply_text(f"Hola {user.first_name}! te encuentras registradx para jugar :D\n\n" + help_text)
 
 
 def change_game(update: Update, context: CallbackContext):
@@ -30,23 +36,24 @@ def change_game(update: Update, context: CallbackContext):
 
     try:
         [ _, new_game_id ] = update.message.text.split(' ')
-        cur.execute(f"UPDATE friends SET game_id={new_game_id} WHERE id={update.message.from_user.id}")
-        update.message.reply_text(f"Ahora estás participando en el juego {new_game_id}")
+        game_title = cur.execute(f"SELECT title FROM games WHERE game_id={int(new_game_id)}").fetchone()[0]
+        cur.execute(f"UPDATE friends SET game_id={int(new_game_id)} WHERE user_id={update.message.from_user.id}")
+        update.message.reply_text(f"Ahora estás participando en el juego: {game_title}")
     except:
-        update.message.reply_text("Hubo un error procesando tu mensaje, por favor intenta de nuevo: /cambiar_juego <id_juego>")
+        update.message.reply_text("Hubo un error procesando tu mensaje, por favor intenta de nuevo: /cambiarJuego <id_juego>")
 
 def add_like(update: Update, context: CallbackContext):
     con = connect(DATABASE)
     cur = con.cursor()
 
     user_id = update.message.from_user.id
-    possible_gift = update.message.text.split(' ')[1:].join(' ')
+    possible_gift = ' '.join(update.message.text.split(' ')[1:])
     last_like = cur.execute(f"SELECT user_like_id FROM likes WHERE user_id={user_id} ORDER BY user_like_id DESC LIMIT 1").fetchone()
     if last_like is None:
         user_like_id = 1
     else:
         user_like_id = int(last_like[0]) + 1
-    cur.execute(f"INSERT INTO likes VALUES({user_id}, {user_like_id}, {possible_gift})")
+    cur.execute(f"INSERT INTO likes VALUES ({user_id}, {user_like_id}, '{possible_gift}')")
     con.commit()
 
 def add_dislike(update: Update, context: CallbackContext):
@@ -54,13 +61,13 @@ def add_dislike(update: Update, context: CallbackContext):
     cur = con.cursor()
 
     user_id = update.message.from_user.id
-    pls_do_not = update.message.text.split(' ')[1:].join(' ')
+    pls_do_not = ''.join(update.message.text.split(' ')[1:])
     last_dislike = cur.execute(f"SELECT user_dislike_id FROM dislikes WHERE user_id={user_id} ORDER BY user_dislike_id DESC LIMIT 1").fetchone()
     if last_dislike is None:
         user_like_id = 1
     else:
         user_like_id = int(last_dislike[0]) + 1
-    cur.execute(f"INSERT INTO likes VALUES({user_id}, {user_like_id}, {pls_do_not})")
+    cur.execute(f"INSERT INTO likes VALUES({user_id}, {user_like_id}, '{pls_do_not}')")
     con.commit()
     
 
--- setup.py
+++ setup.py
@@ -7,6 +7,10 @@ def setup():
     cur.execute("CREATE TABLE friends (user_id, name, game_id, sk, pk)")
     cur.execute("CREATE TABLE likes (user_id, user_like_id, description)")
     cur.execute("CREATE TABLE dislikes (user_id, user_dislike_id, description)")
+    cur.execute("CREATE TABLE games (game_id, title)")
+
+    cur.execute(f"INSERT INTO games VALUES(1, 'Juego de Prueba')")
+    cur.execute(f"INSERT INTO games VALUES(17, 'Ameko Secreto 2023')")
 
     con.commit()
 
